84154e73af68c30b0f32cc9cf077370145f53dff
Paul Dixon <chris_paul_dixon@hotmail.com>
2018-06-07 17:00:54 +0200

    Initial commit

---
+++ Assets/scripts/PersistantManager.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersistantManager : MonoBehaviour {
+
+    public static PersistantManager Instance { get; private set; }
+
+    
+    bool hasPrefabs = false;
+    bool hasHud = false;
+    
+    public GameObject hudView;
+    public GameObject ssrpStatusView;
+    public GameObject internetStatusView;
+    public GameObject workingStatusView;
+
+    private BinaryStateIcon _ssrpIcon;
+    private BinaryStateIcon _connectIcon;
+    private BinaryStateIcon _workingIcon;
+    private SSRP_hud_controller _hud;
+
+    private List<MVC_entity> prefabList;
+
+    /*
+       peopleToThank = new List<string>(new string[] {
+       "[dev] Paul Dixon (paul.dixon@northkingdom)",
+       "[LTU] LTU",
+       "[SSRP] Sense Smart Region platform",
+       "[icon] Rami McMin ( https://www.flaticon.com/authors/rami-mcmin )",
+        "[icon] Eleonor Wang(https://www.flaticon.com/authors/eleonor-wang )",
+        "[icon] https://www.flaticon.com/"
+         });*/
+
+    public bool prefabCheck(List<MVC_entity> prefabList)
+    {
+
+        foreach (MVC_entity entity in prefabList)
+        {
+            if (entity.go == null)
+            {
+               Debug.Log("Prefab not set");
+
+                return false;
+            }
+            /*
+            if (entity.scripts == null)
+            {
+                return false;
+            }
+            */
+        }
+        return true;
+    }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+
+            Instance = this;
+            init();
+            
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    
+    public BinaryStateIcon connectIcon
+    {
+        get
+        {
+            return _connectIcon;
+        }
+
+       
+    }
+   
+
+    public BinaryStateIcon ssrpIcon
+    {
+        get
+        {
+            return _ssrpIcon;
+        }
+        
+    }
+
+    public BinaryStateIcon workingIcon
+    {
+        get
+        {
+            return _workingIcon;
+        }
+        
+    }
+
+    public SSRP_hud_controller hud
+    {
+        get
+        {
+            return _hud;
+        }
+    }
+
+    
+    private void init()
+    {
+        
+
+
+        hasHud = false;
+        try
+        {
+            _hud = hudView.GetComponent<SSRP_hud_controller>();
+            _ssrpIcon = ssrpStatusView.GetComponent<BinaryStateIcon>();
+            _connectIcon = internetStatusView.GetComponent<BinaryStateIcon>();
+            _workingIcon = workingStatusView.GetComponent<BinaryStateIcon>();
+            hasHud = true;
+            Debug.Log("Hud Found, debugging to commence there");
+            hud.addText("HUD active");
+        }
+        catch
+        {
+            Debug.LogWarning("Hud and icons not found");
+        }
+            
+       
+        
+
+            //hud.addText("[peopleToThank]");
+            /*
+            foreach (string person in peopleToThank)
+            {
+                hud.addText("Thanks to: ");
+            }
+            // */
+
+        
+    }
+
+   
+
+}
---
+++ Assets/scripts/SSRP_target_controller.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SSRP_target_controller : MonoBehaviour {
+    PersistantManager boss;// = PersistantManager.Instance;
+
+    // when the Sensor is within Marker distances (within 15m)
+    // we allow the the sensorManager to swap over from using icons as visual tracking to an actual marker.
+    // we spawn in the market using this. Prefab with the specific Marker ID given to us from the sensor data itself.
+    // we can compar this to the Marker DB and generate the expected marker to track.
+    // This also provides and anchor point for adding the Sensor Information.
+
+    public GameObject infoPrefab;
+    public GameObject MarkerPrefab;
+    private List<MVC_entity> prefabList;
+
+    bool isMarkerTractedAndActive = false;
+    bool hasPrefabs = false;
+
+    
+
+    // Use this for initialization
+    void Start () {
+        boss = PersistantManager.Instance;
+        prefabList = new List<MVC_entity> { new MVC_entity(infoPrefab) };
+        //prefabList.Add(new MVC_entity(MarkerPrefab));
+        hasPrefabs = boss.prefabCheck(prefabList);
+        if (hasPrefabs)
+        { 
+            boss.hud.addText("SSRP Target Controller - hasPrefabs: " + hasPrefabs);
+        }
+    }
+	
+    public void initFor(string marker_id)
+    {
+
+        // access Vuforia;
+
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
